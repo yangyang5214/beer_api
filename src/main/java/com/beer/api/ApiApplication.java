ccaa5ab7d1da2be65b29539dab697ccdba23b005
beer <huohuo5214@126.com>
2019-06-23 20:04:12 +0800

    init project

---
+++ src/main/java/com/beer/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.beer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther beer
+ * @date 2019/06/23
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/beer/api/controner/ApiController.java
@@ -0,0 +1,14 @@
+package com.beer.api.controner;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author beer
+ * @date 2019/6/23
+ */
+@RestController
+public class ApiController {
+
+
+        //https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1
+}
